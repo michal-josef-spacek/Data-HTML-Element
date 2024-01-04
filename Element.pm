bd89f6409a42de669a34db0bb361fd5eb30bae54
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-01-04 22:31:23 +0100

    First version

---
+++ Element.pm
@@ -0,0 +1,54 @@
+package Data::HTML::Element;
+
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Data::HTML::Element - Data objects for HTML elements.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Data::HTML::Element::A>
+
+Data object for HTML a element.
+
+=item L<Data::HTML::Element::Button>
+
+Data object for HTML button element.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Data-HTML-Element>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2022-2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
